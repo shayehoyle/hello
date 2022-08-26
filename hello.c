25bfcfac2119c6c95e03e35ef89a33cfab29acf8
Shaye Hoyle <shoyle2@huskers.unl.edu>
2022-08-26 01:17:27 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+*  Authors: Shaye Hoyle, Carter Demuth
+*  Date: 8/25/2022
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World, I am Shaye Hoyle!\n");
+    printf("My major os Undecided.\n");
+    return 0;
+    
+}
