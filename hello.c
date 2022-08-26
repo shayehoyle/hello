30b6ea3cc7e75d68c38cfc1bcdf639f82f48ac84
Carter Demuth <cdemuth4@huskers.unl.edu>
2022-08-26 01:52:43 +0000

    Fri, Aug 26, 2022, 1:52 AM +00:00

--- hello.c
+++ hello.c
@@ -11,8 +11,8 @@ int main(int argc, char **argv) {
     printf("Hello World, I am Shaye Hoyle!\n");
     printf("My major is Undecided.\n");
     printf("Hello World, I am Carter Demuth!\n");
-    printf("My marjor is Computer Engineering.\n");
-    
+    printf("My major is Computer Engineering.\n");
+
     return 0;
 
 }
